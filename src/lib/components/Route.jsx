ef222f0c0665a101e9a5a9cbef97dcecdecd928c
maikCyphlock <maikolaguilar656@gmail.com>
2023-05-22 11:24:58 -0400

    refactor: :art: Add JSDoc comments to Route component
    Add JSDoc comments to Route component  This commit adds JSDoc comments to the Route component, which was previously lacking documentation. The new comments describe the purpose of the function, its parameters, and its return value.  By adding JSDoc comments, we can better communicate the purpose and usage of the Route component to other developers who may work on this code in the future. This will help ensure that the component is used correctly and consistently throughout the application.  There are no changes to the functionality of the component itself, only the addition of comments.

--- src/lib/components/Route.jsx
+++ src/lib/components/Route.jsx
@@ -1,4 +1,11 @@
-
+/**
+ * Returns null.
+ *
+ * @param {string} path - The URL path of the route.
+ * @param {React.ComponentType} component - The component to render for the route.
+ * @param {...any} rest - The rest of the props for the component.
+ * @return {null} Always returns null.
+ */
 function Route ({ path, component: Component, ...rest }) {
   return (
     null
